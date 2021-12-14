ed398a261ed76005d188bab30dc2810d44ffc35d
Baptiste Choquet <baptiste.choquet@pm.me>
2021-12-14 17:11:50 +0100

    Commit Widgets

--- migrations/Version20211214135924.php
+++ migrations/Version20211214135924.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211214084630 extends AbstractMigration
+final class Version20211214135924 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,16 @@ final class Version20211214084630 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE blank_page (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE template (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(20) NOT NULL, description VARCHAR(255) DEFAULT NULL, contenu LONGBLOB NOT NULL, rating INT DEFAULT NULL, type VARCHAR(255) NOT NULL, tag LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE widget (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(20) NOT NULL, contenu LONGBLOB NOT NULL, description VARCHAR(255) NOT NULL, rating INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE blank_page');
         $this->addSql('DROP TABLE template');
+        $this->addSql('DROP TABLE widget');
     }
 }
--- src/Controller/TabletopInfiniteController.php
+++ src/Controller/TabletopInfiniteController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\BlankPageRepository;
 
 class TabletopInfiniteController extends AbstractController
 {
     /**
-    * @Route("/home")
+    *@Route("/home", name="home")
     */
     public function index(): Response
     {
@@ -17,4 +18,16 @@ class TabletopInfiniteController extends AbstractController
             'controller_name' => 'RollTest',
         ]);
     }
+
+    /**
+     * @Route("home/characterSheet")
+     */
+    public function characterSheet(BlankPageRepository $blankPageRepository): Response
+    {
+        return $this->render('blank_page/index.html.twig', [
+            'blank_pages' => $blankPageRepository->findAll(),
+        ]);
+    }
 }
+
+
