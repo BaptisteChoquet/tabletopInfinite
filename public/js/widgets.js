70594b830fbe902ecd7528a3bd467ae6fa62d54a
Baptiste Choquet <baptiste.choquet@pm.me>
2021-12-15 09:51:39 +0100

    Commit DragNDrop

--- public/js/widgets.js
+++ public/js/widgets.js
@@ -4,3 +4,17 @@ for (let i = 0; i < newWidget.length; i++) {
 
     
 }
+
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData(widget, ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData(widget);
+    ev.target.appendChild(document.getElementById(data));
+  }
