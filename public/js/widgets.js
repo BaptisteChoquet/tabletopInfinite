68150d5b253128411b657e10a4a0e608276667dd
Kieran <belli.kieran@gmail.com>
2022-02-28 16:36:40 +0100

    barre de recherche widget

--- public/js/widgets.js
+++ public/js/widgets.js
@@ -1,52 +1,59 @@
-
-
 let newWidget = document.getElementsByClassName("Widget");
 for (let i = 0; i < newWidget.length; i++) {
   let test = newWidget[i].innerText;
-    
-  newWidget[i].innerHTML = content;
 
+  newWidget[i].innerHTML = content;
 }
 
 //Allows to drag and drop elements from the widget list to the character sheet
 function allowDrop(ev) {
-    ev.preventDefault();
-  }
-  
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
+  ev.preventDefault();
+}
 
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
 
-function editWidget(widget){
-    let valueModifier = document.getElementsByClassName("hide");
-    valueModifier[0].style.display = "flex";
-    
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+function editWidget(widget) {
+  let valueModifier = document.getElementsByClassName("hide");
+  valueModifier[0].style.display = "flex";
 }
 
 let IsBorderActive = "true";
-function apercu(){
+function apercu() {
   let widgets = document.getElementsByClassName("widget");
-  if(IsBorderActive == "true"){
-    
-    for(let i = 0;i <widgets.length;i++){
+  if (IsBorderActive == "true") {
+    for (let i = 0; i < widgets.length; i++) {
       widgets[i].style.border = "none";
-      
     }
-    IsBorderActive = "false"
-  }else{
-    
-    for(let i = 0;i <widgets.length;i++){
+    IsBorderActive = "false";
+  } else {
+    for (let i = 0; i < widgets.length; i++) {
       widgets[i].style.border = "solid";
     }
-    IsBorderActive = "true"
+    IsBorderActive = "true";
   }
-  
-  
 }
+
+let allWidgets = document.getElementsByClassName("widget_container");
+let input = document.getElementById("search");
+console.log(input);
+
+input.addEventListener("input", e => {
+  console.log("salut");
+  let search = e.target.value;
+  for (let i = 0; i < allWidgets.length; i++) {
+    let name = document.getElementsByClassName("name");
+    if (!name[i].innerText.toLowerCase().includes(search.toLowerCase())) {
+      allWidgets[i].style.display = "none";
+    } else {
+      allWidgets[i].style.display = "block";
+    }
+  }
+});
