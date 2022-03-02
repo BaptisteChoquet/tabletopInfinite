e70e67f16778401df99ee8a9a5db7e504461f93d
Paul Roumy <paul.roumy@edu.itescia.fr>
2022-03-02 11:22:28 +0100

    favory implémentation

--- src/Controller/SheetCreatorController.php
+++ src/Controller/SheetCreatorController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Favory;
+use App\Entity\Users;
+use App\Entity\Widget;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +14,16 @@ use App\Repository\WidgetRepository;
 class SheetCreatorController extends AbstractController
 {
     #[Route('/sheet_creator', name: 'sheet_creator')]
-    public function index(WidgetRepository $doctrine): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
-        $repository = $doctrine->findAll();
+        $widgets = $doctrine->getRepository(Widget::class)->findAll();
+        $users = $this->getUser();
+        $favorys = $doctrine->getRepository(Favory::class)->findby(['users'=>$users]);
         return $this->render('blank_page/index.html.twig', [
             'controller_name' => 'SheetCreatorController',
-            'widgets' => $repository
+            'widgets' => $widgets,
+            'favorys' => $favorys
         ]);
     }
 }
